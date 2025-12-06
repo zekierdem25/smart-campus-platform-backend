a029cb2427d1eb5ad6ff5534655bd9d7c49fc5c5
zekierdem25 <durgun.erdemzeki@gmail.com>
2025-12-06 23:33:05 +0300

    fix: DesignTimeDbContextFactory'de MySqlServerVersion using hatası düzeltildi

--- backend/Data/DesignTimeDbContextFactory.cs
+++ backend/Data/DesignTimeDbContextFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql;
 
 namespace SmartCampus.API.Data;
 
@@ -21,7 +22,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<Applicatio
             ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found in appsettings.json");
         
         // MySQL 8.0 sürümünü manuel belirt (AutoDetect yerine)
-        var serverVersion = new Pomelo.EntityFrameworkCore.MySql.Infrastructure.MySqlServerVersion(new Version(8, 0, 0));
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 0));
         optionsBuilder.UseMySql(connectionString, serverVersion);
 
         return new ApplicationDbContext(optionsBuilder.Options);
